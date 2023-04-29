f875d4fc2b99d7712e5deea9533797ed779a6dae
Dmitry <mitolbir@gmail.com>
2023-04-29 16:08:29 +0300

    Stepik1 added

---
+++ Поколение Python. Курс для начинающих/Урок 1. Ввод - вывод данных.py
@@ -0,0 +1,43 @@
+# print("Мы изучаем язык Python")
+
+# print("Soon I", "will programming", "on", "Python")
+
+# print("Здравствуй, мир!")
+
+# print("4", "8", "15", "16", "23", "42")
+
+# print("4")
+# print("8")
+# print("15")
+# print("16")
+# print("23")
+# print("42")
+
+# print("*")
+# print("**")
+# print("***")
+# print("****")
+# print("*****")
+# print("******")
+# print("*******")
+
+# name = input()
+# print("Привет,", name)
+
+# club = input()
+# print(club, "- чемпион!")
+
+# word1 = input()
+# word2 = input()
+# word3 = input()
+# print(word1)
+# print(word2)
+# print(word3)
+
+# word1 = input()
+# word2 = input()
+# word3 = input()
+# print(word3)
+# print(word2)
+# print(word1)
+
